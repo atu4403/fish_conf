94dec36a831dd8e296e5add0ce50ca870de32297
atu4403 <73111778+atu4403@users.noreply.github.com>
2022-01-03 12:19:20 +0900

    👻 fix: 様々なバグ修正

--- functions/poetry_pr_merge.fish
+++ functions/poetry_pr_merge.fish
@@ -10,7 +10,8 @@ function poetry_pr_merge --description "https://github.com/atu4403/til/blob/47f2
     end
     and log_title 'pullrequest merge'
     and gh pr merge $release_branch -dm --body "update v$pkg_ver"
-    and git switch -c "v$pkg_ver"_after remotes/origin/main
+    and log_title 'switch new branch'
+    and git switch --no-track -c "v$pkg_ver"_after remotes/origin/main
     and git fetch --prune
     and log_title "create release"
     and set -l release_file dist/(basename $PWD)-$pkg_ver.tar.gz
--- functions/poetry_pre_publish.fish
+++ functions/poetry_pre_publish.fish
@@ -11,14 +11,10 @@ function poetry_pre_publish --description "https://github.com/atu4403/til/blob/4
         return 1
     end
     # checkout
-    git switch -c $release_branch
-    and git pull --rebase origin main
-    # test
-    if ! poetry shell && pytest
-        return 1
-    end
+    git switch -c $release_branch $current_branch
+    # and git pull --rebase origin main # Conflictがあればprでわかるのでpullは行わない
     set current_ver (poetry version -s)
-    echo "現在のバージョンは$current_verです\nどのバージョンにしますか?(major/minor/patch)"
+    echo -e "現在のバージョンは$current_verです\nどのバージョンにしますか?(major/minor/patch)"
     read version_type
     switch $version_type
         case 'ma*'
@@ -44,10 +40,11 @@ function poetry_pre_publish --description "https://github.com/atu4403/til/blob/4
     and log_title 'git add'
     and git add --all
     and log_title 'git commit'
-    and git commit -m "update version $new_version"
+    and git commit --no-verify -m "update version $new_version"
     and log_title 'git push'
     and git push origin $release_branch
     and log_title 'pr create'
     and gh pr create --fill
     and gh pr view --web
+    and echo -e "以下を確認して`poetry_pr_merge`を実行してください\n\t- CIが通ること\n\t- ファイルの差分\n\t- version number\n\t- CHANGELOG"
 end
